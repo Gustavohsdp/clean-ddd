5d937aeec354aef3f0cc83c3dac11ac418278e6e
Gustavo Henrique <gustavohsdp@gmail.com>
2023-05-07 11:59:52 -0300

    feat: :sparkles: create delete question comment use case

---
+++ test/factories/make-question-comment.ts
@@ -0,0 +1,23 @@
+import {
+  QuestionComment,
+  QuestionCommentProps,
+} from '@/domain/forum/enterprise/entities/question-comment'
+import { faker } from '@faker-js/faker'
+import { UniqueEntityID } from './../../src/core/entities/unique-entity-id'
+
+export function makeQuestionComment(
+  override: Partial<QuestionCommentProps> = {},
+  id?: UniqueEntityID,
+) {
+  const questionComment = QuestionComment.create(
+    {
+      authorId: new UniqueEntityID(),
+      questionId: new UniqueEntityID(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
+
+  return questionComment
+}
