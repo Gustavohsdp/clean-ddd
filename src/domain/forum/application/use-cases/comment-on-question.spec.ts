3e1cfa7027eabb0e284b3a1e589729c690f05baf
Gustavo Henrique <gustavohsdp@gmail.com>
2023-05-07 11:32:10 -0300

    feat: :sparkles: create comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,39 @@
+import { CommentOnQuestionUseCase } from './comment-on-question'
+
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-question-repository'
+import { describe, expect, it } from 'vitest'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment On Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'new comment',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'new comment',
+    )
+  })
+})
